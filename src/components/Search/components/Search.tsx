f203ecdfa5d3908ac140f0bb71a30a4c6677317e
lucydct <tenalucia0@gmail.com>
2023-05-25 05:07:47 +0200

    feat:logo

--- src/components/Search/components/Search.tsx
+++ src/components/Search/components/Search.tsx
@@ -7,7 +7,7 @@ export const Search = () => {
 
 	return (
 		<input
-			className="w-4/5 rounded-md h-2 p-4 m-5"
+			className="w-4/5 rounded-md h-2 m-5 bg-white bg-opacity-40 p-8 font-large font-semibold shadow-md shadow-orange-400"
 			type="text"
 			placeholder="Search all the GIFs"
 			{...searchProps}
